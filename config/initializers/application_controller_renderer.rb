38c597a8684602d8e0dc7ada58add19e1c1e7989
wilas0o0o <03mcu30@gmail.com>
2021-11-22 07:44:52 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd46ba06941846e640ef030bd877348582d5a7e206122a4b686268295e5bb5d253c87f87a65ce458a9a69e8cb88d77907f3a182ea28af4a04638c95894d3f4cef'
